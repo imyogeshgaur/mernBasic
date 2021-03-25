9b1eb570d4e1326a8b4368d91610af09cd1479ed
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-03-25 18:15:40 +0530

    day 15

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./App.css"
+import NavBar from "./Components/NavBar"
+import Home from "./Components/Home"
+import About from "./Components/About"
+import Contact from "./Components/Contact"
+import Login from "./Components/Login"
+import SignUp from "./Components/SignUp"
+import {Route} from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+
+      <Route exact path="/">
+        <Home/>
+      </Route>
+
+      <Route exact path="/about">
+        <About/>
+      </Route>
+
+      <Route exact path="/contact">
+        <Contact/>
+      </Route>
+
+      <Route exact path="/login">
+        <Login/>
+      </Route>
+
+      <Route exact path="/signup">
+        <SignUp/>
+      </Route>
+
+    </>
+  )
+}
+
+export default App
