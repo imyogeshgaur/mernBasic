ff3ed79b708f8d4d77fbcb96f7a1a70b599c71de
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-03-30 14:30:36 +0530

    created Contact Page

--- client/src/Components/Contact.jsx
+++ client/src/Components/Contact.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../App.css"
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Hello Contact</h1>
-        </div>
+        <>
+            <h1 className="headingContact">Have Some Query Message Us !!!</h1>
+            <div className="containerFormContact">
+                <form action="/signup" method="POST">
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Name</label>
+                        <input type="text" className="form-control" id="name" name="name" autoComplete="off" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Email</label>
+                        <input type="text" className="form-control" id="email" name="email" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Message</label>
+                        <textarea type="text" className="form-control" rows="5" columns="10" id="phone" name="phone" />
+                    </div>
+                    <button type="submit" className="btnContact">Submit</button>
+                </form>
+            </div>
+        </>
     )
 }
 
