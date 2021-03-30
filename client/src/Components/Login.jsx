40e5cd16b1fed2fea36c511b7f0bb9bf8ddd3752
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-03-30 14:11:18 +0530

    created login and signup pages

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../App.css"
 
 const Login = () => {
     return (
-        <div>
-            <h1>Hello Login</h1>
-        </div>
+        <>
+            <h1 className="headingLogin">Get Ready For Awesome Surprises !!!</h1>
+            <div className="containerFormLogin">
+                <form action="/signup" method="POST">
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Email</label>
+                        <input type="text" className="form-control" id="email" name="email" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="password" name="password" />
+                    </div>
+                    <button type="submit" className="btnLogin">Submit</button>
+                </form>
+            </div>
+        </>
     )
 }
 
--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../App.css"
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>Hello Signup</h1>
-        </div>
+        <>
+            <h1 className="headingRegister">Start Your Journey From Here !!!</h1>
+            <div className="containerFormRegister">
+                <form action="/signup" method="POST">
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Name</label>
+                        <input type="text" className="form-control" id="name" name="name" autoComplete="off" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Email</label>
+                        <input type="text" className="form-control" id="email" name="email" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Phone</label>
+                        <input type="text" className="form-control" id="phone" name="phone" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputEmail1">Work</label>
+                        <input type="text" className="form-control" id="work" name="work" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="password" name="password" />
+                    </div>
+                    <div className="form-group">
+                        <label className="labelRegister" for="exampleInputPassword1">CPassword</label>
+                        <input type="password" className="form-control" id="cpassword" name="cpassword" />
+                    </div>
+                    <button type="submit" className="btnRegister">Submit</button>
+                </form>
+            </div>
+        </>
     )
 }
 
