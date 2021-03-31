755b36ad551ff8eea0f1840d5842e3cee6fa6734
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-03-31 10:14:01 +0530

    navbar added

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../App.css"
 
 const NavBar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-custom">
                 <NavLink className="navbar-brand" to="/">Navbar</NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
