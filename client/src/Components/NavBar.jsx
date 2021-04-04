811d686cd1d7e0c06606eb0869ac7414d5cff472
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-04-04 13:51:26 +0530

    day 26

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,38 +1,44 @@
 import React from 'react'
 import { NavLink } from "react-router-dom"
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../yogesh.css"
 
 
 const NavBar = () => {
-    return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-custom">
-                <NavLink className="navbar-brand" to="/">Navbar</NavLink>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/about">About</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/contact">Contact</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/login">Login</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    )
+    const barsClicked = () =>{
+        const bars = document.querySelector('#bars');
+        const close = document.querySelector('#close');
+   
+        bars.style.display = "none";
+        close.style.display = "block";
+     }
+   
+     const closeClicked  = () =>{
+       const bars = document.querySelector('#bars');
+       const close = document.querySelector('#close');
+   
+       bars.style.display = "block";
+       close.style.display = "none";
+   
+     }
+     return (
+       <>
+         <nav>
+           <input type="checkbox" id="check" />
+           <label for="check" class="checkbtn">
+             <i class="fas fa-bars" id="bars" onClick={barsClicked}></i>
+             <i class="fas fa-times" id="close" onClick={closeClicked}></i>
+           </label>
+           <label class="logo">Yogesh Gaur</label>
+           <ul>
+             <li><NavLink style={{textDecoration:"none"}} to="/index">Home</NavLink></li>
+             <li><NavLink style={{textDecoration:"none"}} to="/about">About</NavLink></li>
+             <li><NavLink style={{textDecoration:"none"}} to="/contact">Contact</NavLink></li>
+             <li><NavLink style={{textDecoration:"none"}} to="/signin">Login</NavLink></li>
+             <li><NavLink style={{textDecoration:"none"}} to="/signup">Signup</NavLink></li>
+           </ul>
+         </nav>
+       </>
+     )
 }
 
 export default NavBar
