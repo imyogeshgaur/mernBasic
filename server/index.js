0f3d73101af8af9e76cdf14037b6a519673eae36
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-04-17 08:58:53 +0530

    day 41

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ const PORT = process.env.PORT || 5000;
 const connectDb = require('./Db/conn');
 
 
+connectDb();
+
 app.use(express.json())
 
 app.use(require('./Routes/userRouter'))
 
-connectDb();
 
 app.listen(PORT, () => {
   console.log(`Listning at ${PORT}`);
