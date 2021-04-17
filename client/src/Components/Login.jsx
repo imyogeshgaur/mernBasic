23deb8223e3f525892db9f4ba271b2b118cbe819
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-04-17 08:58:02 +0530

    day 41

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,24 +1,58 @@
-import React from 'react'
-
-
+import React,{useState} from 'react'
+import {useHistory} from "react-router-dom"
 
 const Login = () => {
+  const history = useHistory();
+  const [user, setUser] = useState({
+    email:"",
+    password:""
+  });
+ 
+  let name,value;
+  const handleData = (e) =>{
+    name=e.target.name;
+    value=e.target.value
+
+    setUser({...user,[name]:value});
+  }
+  const getData = async(e) =>{
+      e.preventDefault();
+      const {email,password} = user;
+
+      const response = await fetch('/signin',{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({email,password})
+      });
+
+      const data = response.json();
+
+      if(data.status === 400 || !data){
+        alert("Invalid Credentials !!!");
+      }else{
+        alert("Login Sucessfull !!!");
+        history.push("/");
+      }
+  }
     return (
         <>
-            <h1 className="headingLogin">Get Ready For Awesome Surprises !!!</h1>
-            <div className="containerFormLogin">
-                <form action="/signup" method="POST">
-                    <div className="form-group">
-                        <label className="labelRegister" for="exampleInputEmail1">Email</label>
-                        <input type="text" className="form-control" id="emailSignIn" name="emailSignIn" autoComplete="none" />
-                    </div>
-                    <div className="form-group">
-                        <label className="labelRegister" for="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="password" name="password" />
-                    </div>
-                    <button type="submit" className="btnLogin">Submit</button>
-                </form>
-            </div>
+        <h1 className="headingLogin">Get Ready For Awesome Surprises !!!</h1>
+        <div className="containerFormLogin">
+        <form method="POST">
+        <div className="form-group">
+          <label className="labelRegister" for="exampleInputEmail1">Email</label>
+          <input type="text" className="form-control" id="email" name="email" autoComplete="none"
+          value={user.email} onChange={handleData}/>
+        </div>
+        <div className="form-group">
+          <label className="labelRegister" for="exampleInputPassword1">Password</label>
+          <input type="password" className="form-control" id="password" name="password" value={user.password} onChange={handleData}/>
+        </div>
+        <button type="submit" className="btnLogin" onClick={getData}>Submit</button>
+      </form>
+        </div>  
         </>
     )
 }
